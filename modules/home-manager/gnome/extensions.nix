818fa8b05941a090fd43d546846bda1e877160a8
Jannis <jannis.rustige@gmail.com>
2025-04-26 14:19:11 +0200

    add dash-to-dock

--- modules/home-manager/gnome/extensions.nix
+++ modules/home-manager/gnome/extensions.nix
@@ -11,6 +11,7 @@
         "blur-my-shell@aunetx"
         "caffeine@patapon.info"
         "monitor@astraext.github.io"
+        "dash-to-dock@micxgx.gmail.com"
       ];
     };
 
@@ -18,14 +19,26 @@
       name = "Catppuccin-Dark";
     };
 
+    "org/gnome/shell/extensions/dash-to-dock" = {
+      dock-position = "LEFT";
+      apply-custom-theme = true;
+      extended-height = true;
+      custom-theme-shrink = true;
+      force-straight-corner = true;
+      dock-fixed = true;
+    };
 
+    "org/gnome/shell/extensions/blur-my-shell/dash-to-dock" = {
+      pipeline = "pipeline_default";
+    };
   };
-  # /org/gnome/shell/extensions/user-theme/
+
   home.packages = with pkgs.gnomeExtensions; [
     user-themes
     color-picker
     blur-my-shell
     caffeine
     astra-monitor
+    dash-to-dock
   ];
 }
